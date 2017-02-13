c788b50d1b55d7900ff72add654dec95399130ba
bcollyer <benjamin.collyer@gmail.com>
2017-02-13 12:40:13 +0000

    Update SOR_iteration.jl

--- src/SOR_iteration.jl
+++ src/SOR_iteration.jl
@@ -1,9 +1,6 @@
-@acc function SOR_iteration(img::Array{Float64,2},img0::Array{Float64,2}, iterations::Int)
+@acc function SOR_iteration(img::Array{Float64,2},img0::Array{Float64,2}, iterations::Int, om::Float64=1.0, lambda2::Float64=0.05)
     buf = Array(Float64, size(img)...) 
     runStencil(buf, img, img0, iterations, :oob_wraparound) do b, a, c
-      
-        om = 1.98
-        lambda2=0.05
         
         b[0,0] = (1.0-om)*a[0,0] + om*( a[0,0] + 0.15*(
                                     (-(1/12) * a[0,2] + (4/3) * a[0,1] - (5/2)*a[0,0] + (4/3) * a[0,-1] -(1/12) * a[0,-2])
