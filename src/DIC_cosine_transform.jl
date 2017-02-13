9a551c04ce20f8bfe41cecf1d7d977d70877772d
bcollyer <benjamin.collyer@gmail.com>
2017-02-13 12:02:00 +0000

    Update DIC_cosine_transform.jl

--- src/DIC_cosine_transform.jl
+++ src/DIC_cosine_transform.jl
@@ -2,48 +2,28 @@ using PyPlot, Images
 using PyCall
 @pyimport numpy
 
-function DIC_cosine_transform(img_name)
+function DIC_cosine_transform(img_name,lambda)
 
+    # import image
     raw_img = PyPlot.imread(img_name);
     img = convert(Image{Images.Gray},raw_img[:,:,1:3]);
     M = convert(Array,img);
     M = float(M); 
     
+    #subtract average of each column
     mx, my = size(M)
     m1 = mean(M[:,1])
     mL = mean(M[:,end])
-    
     for i=1:mx
-        #M[:,i]=M[:,i] - i*(mL-m1)/my
         M[:,i] = M[:,i] - mean(M[:,i])
     end
-    
-    m1 = mean(M[:,1])
-    mL = mean(M[:,end])   
-    
-    println(m1)
-    println(mL)
-    
-    #M = [M flipdim(M[:,2:end-1],2)]
-    
-    println(size(M))
+
         
     gX, gY = numpy.gradient(M);  
-    
-        for i=1:mx
-        #M[:,i]=M[:,i] - i*(mL-m1)/my
-        gY[:,i] = gY[:,i] - mean(gY[:,i])
-    end
-   # gY = [gY flipdim(gY[:,2:end-1],2)];
-    
-    println(size(gY))
-    lambda = 0.1;
     d1 = FFTW.r2r(gY,FFTW.REDFT00)
     nx, ny = size(d1)
-    
-    println(size(d1))
     d2=zeros(ny,nx)
-    d2=[(nx/pi)^2 * d1[j,i]/(lambda*j^(1.25) + i^2) for i in 1:ny, j in 1:nx];
+    d2=[(nx/pi)^2 * d1[j,i]/(lambda*j^(2.0) + i^2) for i in 1:ny, j in 1:nx];
     #d2=[(nx/pi)^2 * d1[j,i] for i in 1:ny, j in 1:nx];
     
     dst = convert(Array{Float64,2},d2)
@@ -52,11 +32,10 @@ function DIC_cosine_transform(img_name)
     
     maxi = maximum(e1)
     mini = minimum(e1)
-    
     clamped = 1 +  (-e1 + maxi) * 1 / ( mini-maxi);
+    out_name = split(image_name,".")[1] * "_cos_l=$(@sprintf("%.2f", lambda))" * ".png"
+    imwrite(grayim(clamped),out_name)
     
-    imwrite(grayim(clamped),"cosDICdan.png")
-    
-    return e1
+    return 0
     
 end
